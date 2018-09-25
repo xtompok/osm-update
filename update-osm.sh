412e59636bf7d09e7447cb2543e7ac1d16539849
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2018-09-25 12:57:35 +0200

    Overwrite existing data and add raw data.

--- update-osm.sh
+++ update-osm.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 wget -N "http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf" 
-ogr2ogr -f PostgreSQL "PG:dbname=cz_osm active_schema=ogr" czech-republic-latest.osm.pbf \
+ogr2ogr -f PostgreSQL "PG:dbname=cz_osm" czech-republic-latest.osm.pbf \
     -lco COLUMN_TYPES=other_tags=hstore \
-        --config OSM_MAX_TMPFILE_SIZE 1024
+    -overwrite \
+    --config OSM_MAX_TMPFILE_SIZE 1024
 
+osmosis --read-pbf czech-republic-latest.osm.pbf --log-progress --write-pgsql database=cz_osm user=j́ethro password=UBERPASSWORD
 
